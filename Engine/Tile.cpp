7c46ef4cf876ec4147d13843bcf970e5a7c22215
kathelas <athelaswow@gmail.com>
2018-03-28 18:19:37 +0200

    changed draw function to draw bombs

--- Engine/Tile.cpp
+++ Engine/Tile.cpp
@@ -3,7 +3,17 @@
 
 void Tile::Draw( const Vei2& screenpos, Graphics& gfx )
 {
-	SpriteCodex::DrawTileButton( screenpos, gfx );
+	switch( state )
+	{
+	case State::Hidden:
+		SpriteCodex::DrawTileButton( screenpos, gfx );
+		break;
+	case State::Revealed:
+		SpriteCodex::DrawTile0( screenpos, gfx );
+		if( hasBomb )
+			SpriteCodex::DrawTileBomb;
+		break;
+	}
 }
 
 bool Tile::HasBomb()
